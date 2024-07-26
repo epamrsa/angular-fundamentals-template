36a4da248d6c8d36120dbcf039abb1d4d7c0ee0a
epamrsa <epamrsa@gmail.com>
2024-07-26 17:06:10 +0300

    impl: Implemented (partially) Module-3 Task-4

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {
-  FormBuilder, FormControl, FormGroup, Validators
+  FormBuilder, Validators
 } from '@angular/forms';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -15,20 +15,32 @@ export class CourseFormComponent {
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
   }
-  courseForm = new FormGroup({
+  courseForm = this.fb.group({
     // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
-    title: new FormControl("", [Validators.required, Validators.minLength(2)]),
-    description: new FormControl("", [Validators.required, Validators.minLength(2)]),
-    author: new FormControl("", [Validators.minLength(2), authorValidator(2)]),
-    duration: new FormControl(0),
+    title: ["", Validators.required, Validators.minLength(2)],
+    description: ["", [Validators.required, Validators.minLength(2)]],
+    author: ["", [Validators.minLength(2), authorValidator(2)]],
+    authors: this.fb.array([]),
+    duration: ["", [Validators.required, Validators.min(0)]]
   });
   submitted = false;
 
   courseDurationFormatted() {
-    let duration = this.courseForm.controls.duration.value;
-    if (duration == null) return "";
+    let duration = this.courseForm.controls.duration.value ? parseInt(this.courseForm.controls.duration.value) : null;
+    if (duration == null || duration < 0) return "";
     if (duration < 60) return duration + " minutes";
     return Math.floor(duration / 60) + ":" + duration % 60 + " hours";
   }
+
+  createAuthor() {
+    if (this.courseForm.controls.authors.valid) {
+      this.courseForm.controls.authors.push(this.fb.control(this.courseForm.controls.author.value));
+      this.courseForm.controls.author.setValue("");
+    }
+  }
+
+  addToCourseAuthors(index: number) {}
+
+  removeFromCourseAuthors(index: number) {}
 
 }
